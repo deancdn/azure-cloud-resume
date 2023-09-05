be0013fb9042d268d5678e419c7cb37d80120ea2
Dean Chia <deanchia@Deans-MacBook-Air.local>
2023-09-05 22:08:46 +0800

    third commit

--- visitorcount/HttpTrigger1/index.js
+++ visitorcount/HttpTrigger1/index.js
@@ -3,7 +3,7 @@ const { CosmosClient } = require("@azure/cosmos");
 const endpoint = "https://visitor-count-azure-resume.documents.azure.com:443/";
 const key = "9kQmgGOyKOUF7Mcdy964ZV0BTVN3IPTVmVrxc91eqBNgqyvk2pLz1M45pyYAlnfLQZMv0P2D6VhcACDbx7pnJA==";
 const databaseId = "VisitorCount";
-const containerId = "VisiorCount";
+const containerId = "VisitorCount";
 
 const client = new CosmosClient({ endpoint, key });
 const container = client.database(databaseId).container(containerId);
